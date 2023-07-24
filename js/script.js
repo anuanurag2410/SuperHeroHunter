4e907c59467648ed6e5db67ae9bf99a8f9e286d2
Anurag Srivastava <anuanurag2410@gmail.com>
2023-07-24 15:07:10 +0530

    the code is ready

--- js/script.js
+++ js/script.js
@@ -1,172 +1,200 @@
-// Retrieving the card container from the DOM
-let cardContainer = document.getElementById('container');
+// Retrieve DOM elements
+let searchBar = document.getElementById("search-bar");
+let searchResults = document.getElementById("search-results");
 
-// Event listener attached to the window to execute the code when the page is loaded
-window.addEventListener("load", function () {
-     // Getting the favoriteCharacters array from localStorage
-     let favorites = localStorage.getItem("favoriteCharacters");
+// Add event listener to the search bar
+searchBar.addEventListener("input", () => searchHeroes(searchBar.value));
 
-     // If favorites is null, display a message and return
-     if (favorites == null) {
-          cardContainer.innerHTML = "<p class=\"no-characters\">No characters present in Favorites</p>";
-          return;
-     }
-     // If favorites is not null, parse it to convert it into an array
-     else {
-          favorites = JSON.parse(localStorage.getItem("favoriteCharacters"));
-     }
+// Function to search superheroes using the Marvel API
+async function searchHeroes(searchText) {
+    if (searchText.length === 0) {
+        searchResults.innerHTML = ``;
+        return;
+    }
 
-     // If all the characters are deleted from favorites and there are no characters left to display
-     if (favorites.length === 0) {
-          cardContainer.innerHTML = "<p class=\"no-characters\">No characters present in Favorites</p>";
-          return;
-     }
+    // Marvel API credentials
+    const PUBLIC_KEY = "9ab871748d83ae2eb5527ffd69e034de";
+    const PRIVATE_KEY = "ad79003cf7316d9bd72c6eda71d1c93d7e807e90";
+    const timestamp = new Date().getTime();
+    const hash = "d35377547e551cd64a60657d2517bb7f"; // MD5 hash
 
-     cardContainer.innerHTML = "";
-     favorites.forEach(character => {
-          cardContainer.innerHTML +=
-               `
-               <div class="flex-col card">
-                    <img src="${character.squareImage}" alt="">
-                    <span class="name">${character.name}</span>
-                    <span class="id">Id : ${character.id}</span>
-                    <span class="comics">Comics : ${character.comics}</span>
-                    <span class="series">Series : ${character.series}</span>
-                    <span class="stories">Stories : ${character.stories}</span>
-                    <a class="character-info" href="./more-info.html">
-                         <button class="btn"><i class="fa-solid fa-circle-info"></i> &nbsp; More Info</button>
-                    </a>
-                    <div style="display:none;">
-                         <span>${character.id}</span>
-                         <span>${character.name}</span>
-                         <span>${character.comics}</span>
-                         <span>${character.series}</span>
-                         <span>${character.stories}</span>
-                         <span>${character.description}</span>
-                         <span>${character.landscapeImage}</span>
-                         <span>${character.portraitImage}</span>
-                         <span>${character.squareImage}</span>
+    try {
+        // Fetch data from the Marvel API using the provided search text, public key, hash, and timestamp
+        const response = await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchText}&apikey=${PUBLIC_KEY}&hash=${hash}&ts=${timestamp}`);
+        const data = await response.json();
+
+        // Display the search results
+        showSearchResults(data.data.results);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+// Function to display the search results in the DOM
+function showSearchResults(heroes) {
+    searchResults.innerHTML = ``;
+    let count = 1;
+
+    // Loop through the searched heroes and display up to 5 results
+    for (const hero of heroes) {
+        if (count > 5) break;
+
+        // Create HTML elements to display each superhero's information
+        searchResults.innerHTML +=
+            `
+            <li class="flex-row single-search-result">
+                <div class="flex-row img-info">
+                    <img src="${hero.thumbnail.path + '/portrait_medium.' + hero.thumbnail.extension}" alt="">
+                    <div class="hero-info">
+                        <a class="character-info" href="./more-info.html">
+                            <span class="hero-name">${hero.name}</span>
+                        </a>
                     </div>
-                    <button class="btn remove-btn"><i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favorites</button>
-               </div>
-          `;
-     });
-     // Adding appropriate event listeners to the buttons after they are inserted into the DOM
-     addEvent();
-});
+                </div>
+                <div class="flex-col buttons">
+                    <button class="btn add-to-fav-btn">${getFavButtonIcon(hero.id)} &nbsp; ${getFavButtonText(hero.id)}</button>
+                </div>
+                <div style="display:none;">
+                    <span>${hero.name}</span>
+                    <span>${hero.description}</span>
+                    <span>${hero.comics.available}</span>
+                    <span>${hero.series.available}</span>
+                    <span>${hero.stories.available}</span>
+                    <span>${hero.thumbnail.path + '/portrait_uncanny.' + hero.thumbnail.extension}</span>
+                    <span>${hero.id}</span>
+                    <span>${hero.thumbnail.path + '/landscape_incredible.' + hero.thumbnail.extension}</span>
+                    <span>${hero.thumbnail.path + '/standard_fantastic.' + hero.thumbnail.extension}</span>
+                </div>
+            </li>
+        `;
 
-// Function for attaching event listeners to buttons
-function addEvent() {
-     let removeBtn = document.querySelectorAll(".remove-btn");
-     removeBtn.forEach((btn) => btn.addEventListener("click", removeCharacterFromFavorites));
+        count++;
+    }
 
-     let characterInfo = document.querySelectorAll(".character-info");
-     characterInfo.forEach((character) => character.addEventListener("click", addInfoInLocalStorage));
+    // Attach events to the buttons after they are inserted in the DOM
+    attachButtonEvents();
 }
 
-function removeCharacterFromFavorites() {
-     // Storing the ID of the character to be deleted in a variable
-     let idOfCharacterToBeDeleted = this.parentElement.children[2].innerHTML.substring(5);
+// Function to attach event listeners to buttons
+function attachButtonEvents() {
+    let favouriteButton = document.querySelectorAll(".add-to-fav-btn");
+    favouriteButton.forEach((btn) => btn.addEventListener("click", addToFavourites));
 
-     // Getting the favorites array that stores objects of characters
-     let favorites = JSON.parse(localStorage.getItem("favoriteCharacters"));
-     // favoritesCharacterIDs is taken from localStorage for deleting the ID of the character that is deleted from favorites
-     let favoritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favoritesCharacterIDs")));
-     // Deleting the character's ID from favoritesCharacterIDs map
-     favoritesCharacterIDs.delete(`${idOfCharacterToBeDeleted}`);
-
-     // Deleting the character from the array whose ID matches
-     favorites.forEach(function (favorite, index) {
-          if (favorite.id == idOfCharacterToBeDeleted) {
-               favorites.splice(index, 1);
-          }
-     });
-
-     // If all the characters are deleted from favorites and there are no characters left to display
-     if (favorites.length === 0) {
-          cardContainer.innerHTML = "<p class=\"no-characters\">No characters present in Favorites</p>";
-     }
-
-     // Updating the new arrays in localStorage
-     localStorage.setItem("favoriteCharacters", JSON.stringify(favorites));
-     localStorage.setItem("favoritesCharacterIDs", JSON.stringify([...favoritesCharacterIDs]));
-
-     // Removing the element from the DOM
-     this.parentElement.remove();
-
-     // Displaying the "Removed from favorites" toast in the DOM
-     document.querySelector(".remove-toast").setAttribute("data-visiblity", "show");
-     // Removing the "Removed from favorites" toast from the DOM
-     setTimeout(function () {
-          document.querySelector(".remove-toast").setAttribute("data-visiblity", "hide");
-     }, 1000);
+    let characterInfo = document.querySelectorAll(".character-info");
+    characterInfo.forEach((character) => character.addEventListener("click", addInfoInLocalStorage));
 }
 
-// Function that stores the info object of the character for which the user wants to see the info
+// Function invoked when "Add to Favourites" or "Remove from Favourites" button is clicked, appropriate action is taken based on the button clicked
+function addToFavourites() {
+    // Get the hero ID of the character to be added/removed
+    let heroId = this.parentElement.parentElement.children[2].children[6].innerHTML;
+
+    // Get the favouritesCharacterIDs from localStorage
+    let favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+
+    if (!favouritesCharacterIDs.has(heroId)) {
+        // If the character ID is not in favourites, add it
+        favouritesCharacterIDs.set(heroId, true);
+        addToFavouritesArray(heroId);
+        this.innerHTML = `<i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favourites`;
+
+        // Show "Added to Favourites" toast
+        showToast("Added to Favourites");
+    } else {
+        // If the character ID is in favourites, remove it
+        favouritesCharacterIDs.delete(heroId);
+        removeFromFavouritesArray(heroId);
+        this.innerHTML = `<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites`;
+
+        // Show "Remove from Favourites" toast
+        showToast("Removed from Favourites");
+    }
+
+    // Save the updated favouritesCharacterIDs in localStorage
+    localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+}
+
+// Function to add the character info to the favourites array in localStorage
+function addToFavouritesArray(heroId) {
+    let heroInfo = {
+        name: this.parentElement.parentElement.children[2].children[0].innerHTML,
+        description: this.parentElement.parentElement.children[2].children[1].innerHTML,
+        comics: this.parentElement.parentElement.children[2].children[2].innerHTML,
+        series: this.parentElement.parentElement.children[2].children[3].innerHTML,
+        stories: this.parentElement.parentElement.children[2].children[4].innerHTML,
+        portraitImage: this.parentElement.parentElement.children[2].children[5].innerHTML,
+        id: heroId,
+        landscapeImage: this.parentElement.parentElement.children[2].children[7].innerHTML,
+        squareImage: this.parentElement.parentElement.children[2].children[8].innerHTML
+    };
+
+    // Get the current favourites array from localStorage
+    let favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters")) || [];
+    favouritesArray.push(heroInfo);
+
+    // Save the updated favourites array in localStorage
+    localStorage.setItem("favouriteCharacters", JSON.stringify(favouritesArray));
+}
+
+// Function to remove the character from the favourites array in localStorage
+function removeFromFavouritesArray(heroId) {
+    // Get the current favourites array from localStorage
+    let favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters")) || [];
+
+    // Filter out the character with the specified ID
+    favouritesArray = favouritesArray.filter((hero) => hero.id !== heroId);
+
+    // Save the updated favourites array in localStorage
+    localStorage.setItem("favouriteCharacters", JSON.stringify(favouritesArray));
+}
+
+// Function to display a toast message for adding/removing from favourites
+function showToast(message) {
+    let toast = document.querySelector(`.${message.toLowerCase()}-toast`);
+    toast.setAttribute("data-visibility", "show");
+    setTimeout(() => {
+        toast.setAttribute("data-visibility", "hide");
+    }, 1000);
+}
+
+// Function to store the character info in localStorage when the user wants to see more info
 function addInfoInLocalStorage() {
-     // This function basically stores the data of the character in localStorage.
-     // When the user clicks on the info button and the info page is opened, that page fetches the heroInfo and displays the data
-     let heroInfo = {
-          name: this.parentElement.children[7].children[1].innerHTML,
-          description: this.parentElement.children[7].children[5].innerHTML,
-          comics: this.parentElement.children[7].children[2].innerHTML,
-          series: this.parentElement.children[7].children[3].innerHTML,
-          stories: this.parentElement.children[7].children[4].innerHTML,
-          portraitImage: this.parentElement.children[7].children[7].innerHTML,
-          id: this.parentElement.children[7].children[0].innerHTML,
-          landscapeImage: this.parentElement.children[7].children[6].innerHTML
-     };
+    let heroInfo = {
+        name: this.parentElement.parentElement.parentElement.children[2].children[0].innerHTML,
+        description: this.parentElement.parentElement.parentElement.children[2].children[1].innerHTML,
+        comics: this.parentElement.parentElement.parentElement.children[2].children[2].innerHTML,
+        series: this.parentElement.parentElement.parentElement.children[2].children[3].innerHTML,
+        stories: this.parentElement.parentElement.parentElement.children[2].children[4].innerHTML,
+        portraitImage: this.parentElement.parentElement.parentElement.children[2].children[5].innerHTML,
+        id: this.parentElement.parentElement.parentElement.children[2].children[6].innerHTML,
+        landscapeImage: this.parentElement.parentElement.parentElement.children[2].children[7].innerHTML,
+        squareImage: this.parentElement.parentElement.parentElement.children[2].children[8].innerHTML
+    };
 
-     localStorage.setItem("heroInfo", JSON.stringify(heroInfo));
+    localStorage.setItem("heroInfo", JSON.stringify(heroInfo));
 }
 
-// Theme Changing
-
-// Selection of theme button
-let themeButton = document.getElementById("theme-btn");
-
-themeButton.addEventListener("click", themeChanger);
-
-// IIFE function which checks the localStorage and applies the previously set theme
-(function () {
-     let currentTheme = localStorage.getItem("theme");
-     if (currentTheme == null) {
-          root.setAttribute("color-scheme", "light");
-          themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
-          themeButton.style.backgroundColor = "#0D4C92";
-          localStorage.setItem("theme", "light");
-          return;
-     }
-
-     switch (currentTheme) {
-          case "light":
-               root.setAttribute("color-scheme", "light");
-               themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
-               themeButton.style.backgroundColor = "#0D4C92";
-               break;
-          case "dark":
-               root.setAttribute("color-scheme", "dark");
-               themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
-               themeButton.style.backgroundColor = "#FB2576";
-               themeButton.childNodes[0].style.color = "black";
-               break;
-     }
-})();
-
-// Function for handling theme button changes
+// Function to toggle between light and dark theme
 function themeChanger() {
-     let root = document.getElementById("root");
-     if (root.getAttribute("color-scheme") == "light") {
-          root.setAttribute("color-scheme", "dark");
-          themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
-          themeButton.style.backgroundColor = "#FB2576";
-          themeButton.childNodes[0].style.color = "black";
-          localStorage.setItem("theme", "dark");
-     } else if (root.getAttribute("color-scheme") == "dark") {
-          root.setAttribute("color-scheme", "light");
-          themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
-          themeButton.style.backgroundColor = "#0D4C92";
-          themeButton.childNodes[0].style.color = "white";
-          localStorage.setItem("theme", "light");
-     }
+    let root = document.getElementById("root");
+    let themeButton = document.getElementById("theme-btn");
+    let currentTheme = localStorage.getItem("theme");
+
+    if (currentTheme == "light") {
+        root.setAttribute("color-scheme", "dark");
+        themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+        themeButton.style.backgroundColor = "#FB2576";
+        themeButton.childNodes[0].style.color = "black";
+        localStorage.setItem("theme", "dark");
+    } else if (currentTheme == "dark") {
+        root.setAttribute("color-scheme", "light");
+        themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+        themeButton.style.backgroundColor = "#0D4C92";
+        themeButton.childNodes[0].style.color = "white";
+        localStorage.setItem("theme", "light");
+    }
 }
+
+// Attach event listener to the theme button
+let themeButton = document.getElementById("theme-btn");
+themeButton.addEventListener("click", themeChanger);
